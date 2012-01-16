ce5c01d5cdbd29bbf1694ca9218a85600651d061
bryan31737@gmail.com <bryan31737@gmail.com@7b555943-497e-1e55-1248-e7a86a691bf8>
2012-01-16 19:52:32 +0000

    New upload for testing

---
+++ Test_1/src/Test.java
@@ -0,0 +1,19 @@
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int thisisaverylondvariable;
+		thisisaverylondvariable = 4;
+		for(int x = 0 ; x <= thisisaverylondvariable; x++){
+			for(int y = 0 ; y < thisisaverylondvariable; y++){
+				System.out.print(y * x);			
+			}
+			System.out.println();
+		}
+			 
+	}
+
+}

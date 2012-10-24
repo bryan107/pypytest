c1a0db596e53541ebfd321f29f0ac376ebfc9e52
bryan31737@gmail.com <bryan31737@gmail.com@7b555943-497e-1e55-1248-e7a86a691bf8>
2012-10-24 17:48:12 +0000

    reupload

--- SmartGridSimulator/src/smartgrid/component/Fault.java
+++
@@ -1,6 +0,0 @@
-package smartgrid.component;
-
-public interface Fault {
-	public double getValue(double value);
-	public boolean isNull();
-}
--- SmartGridSimulator/src/smartgrid/component/Pattern.java
+++
@@ -1,5 +0,0 @@
-package smartgrid.component;
-
-public interface Pattern {
-	public double getValue(double variation, double[][] attribute, long sections, long sectionnumber);
-}

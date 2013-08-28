e12c88cdd72c916909dd535c81271ee24c67eb5c
bryan31737@gmail.com <bryan31737@gmail.com@7b555943-497e-1e55-1248-e7a86a691bf8>
2013-08-28 23:19:19 +0000

    Implementing MGA functions

--- trunk/CMGA/src/mga/core/MGA.java
+++ trunk/CMGA/src/mga/core/MGA.java
@@ -12,8 +12,7 @@ public class MGA {
 
 	private Map<Integer, Node> nodemap = new HashMap<Integer, Node>();
 	private Map<Integer, Map<Double, Integer>> correlationmap = new HashMap<Integer, Map<Double, Integer>>();
-	private Map<Integer, LinkedList<Integer>> correlationgrade = new HashMap<Integer, LinkedList<Integer>>();
-	
+
 	public MGA(Map<Integer, Node> nodemap) {
 		updateNodeMap(nodemap);
 	}
@@ -33,45 +32,94 @@ public class MGA {
 				if (id1 == id2) {
 					continue;
 				}
-				double[] values1 = linkedListToDoubleArray(nodemap.get(id1).values());
-				double[] values2 = linkedListToDoubleArray(nodemap.get(id2).values());
+				double[] values1 = linkedListToDoubleArray(nodemap.get(id1)
+						.values());
+				double[] values2 = linkedListToDoubleArray(nodemap.get(id2)
+						.values());
 				double correlation = Stat.corrCoeff(values1, values2);
-				subcsmap.put(correlation, id2);	
+				subcsmap.put(correlation, id2);
 			}
 			correlationmap.put(id1, subcsmap);
 		}
 	}
-	
-	public void clustering(){
-		setCorrelationGrade();
+
+	public void clustering() {
 		
+
 	}
 
-	private void setCorrelationGrade() {
-		Iterator<Integer> it = correlationmap.keySet().iterator();
-		while(it.hasNext()){
-			int nodeid = it.next();
-			LinkedList<Integer> list = sortMap(correlationmap.get(nodeid));
-			correlationgrade.put(nodeid, list);
+	
+	public Map<Integer, LinkedList<Integer>> pairing(Map<Integer, LinkedList<Integer>> cluster, Map<Integer, LinkedList<Integer>> clustergrade) {
+		Map<Integer, LinkedList<Integer>> pairings = new HashMap<Integer, LinkedList<Integer>>();
+		int index = 0;
+		while(clustergrade.size() != 0){
+			serachPair(cluster, clustergrade, pairings, index);
+		}
+		return pairings;
+	}
+
+	private void serachPair(Map<Integer, LinkedList<Integer>> cluster, Map<Integer, LinkedList<Integer>> clustergrade,
+			Map<Integer, LinkedList<Integer>> pairings, int index) {
+		Iterator<Integer> it = clustergrade.keySet().iterator();
+		while (it.hasNext()) {
+			int id = it.next();
+			if(clustergrade.get(id).peek() == null)
+			{
+				clustergrade.get(id).remove();
+				continue;
+			}
+			int id_2 = clustergrade.get(id).peek();
+			if(clustergrade.get(id_2).peek() == null){
+				continue;
+			}
+			// Check whether the first priority of nodeid_1 is successfully paired.
+			if (id == clustergrade.get(id_2).peek()) {
+				LinkedList<Integer> temp = new LinkedList<Integer>();
+				temp.addAll(cluster.get(id));
+				temp.addAll(cluster.get(id_2));
+				pairings.put(index, temp);
+				clustergrade.remove(id);
+				clustergrade.remove(id_2);
+			}
 		}
 	}
+
+	// Sort each node with their own priority
 	
+	//TODO the grading function
+	private Map<Integer, LinkedList<Integer>> getClusterGrade(Map<Integer, LinkedList<Integer>> cluster) {
+		Map<Integer, LinkedList<Integer>> clustergrade = new HashMap<Integer, LinkedList<Integer>>();
+		Iterator<Integer> it = cluster.keySet().iterator();
+		while(it.hasNext()){
+			int clusterid = it.next();
+			
+		}
+		
+//		Iterator<Integer> it = correlationmap.keySet().iterator();
+//		while (it.hasNext()) {
+//			int nodeid = it.next();
+//			LinkedList<Integer> list = sortMap(correlationmap.get(nodeid));
+//			clustergrade.put(nodeid, list);
+//		}
+		return clustergrade;
+	}
+
 	// Sort Map in a Decent order
-	private LinkedList<Integer> sortMap(Map<Double, Integer> input){
-		LinkedList<Integer> list = new LinkedList<Integer>(); 
+	private LinkedList<Integer> sortMap(Map<Double, Integer> input) {
+		LinkedList<Integer> list = new LinkedList<Integer>();
 		double[] correlation = new double[input.size()];
 		Iterator<Double> it = input.keySet().iterator();
 		int i = 0;
-		while(it.hasNext()){
+		while (it.hasNext()) {
 			correlation[i] = it.next();
 			i++;
 		}
 		// Sort the correlation array in an ascent order
 		Arrays.sort(correlation);
 		// Put node id with regard of the value of correlation in a decent order
-		for(int j = (correlation.length - 1); j >= 0 ; j--){
+		for (int j = (correlation.length - 1); j >= 0; j--) {
 			list.add(input.get(correlation[j]));
-		}	
+		}
 		return list;
 	}
 

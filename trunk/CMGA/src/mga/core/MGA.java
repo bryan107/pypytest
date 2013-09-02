f8b735295c9f10af0bfdb77b61e661c65ad2f488
bryan31737@gmail.com <bryan31737@gmail.com@7b555943-497e-1e55-1248-e7a86a691bf8>
2013-09-02 17:11:23 +0000

    1. Add Properties access functions 2. Split the MGA function into 3 object   - MGA as the main object provides function   - NodeManager manages node data and correlation calculation   - ClusterManager manages the cluster

--- trunk/CMGA/src/mga/core/MGA.java
+++ trunk/CMGA/src/mga/core/MGA.java
@@ -1,77 +1,126 @@
 package mga.core;
 
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Map;
 
-import flanagan.analysis.Stat;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
+import fileAccessInterface.PropertyAgent;
+
+// TODO TEST FUNCTIONS
 public class MGA {
-
-	private Map<Integer, Node> nodemap = new HashMap<Integer, Node>();
-	private Map<Integer, Map<Integer, Double>> correlationmap = new HashMap<Integer, Map<Integer, Double>>();
+	private static Log logger = LogFactory.getLog(MGA.class);
+	// ------------ Data Structures --------------
+	private NodeManager cmanager;
+	private ClusterManager clustermanager;
+	private PropertyAgent agent;
+	private double mincorrelation;
+	private int coption;
+	// -------------------------------------------
 
 	public MGA(Map<Integer, Node> nodemap) {
-		updateNodeMap(nodemap);
+		// init objects
+		cmanager = new NodeManager(nodemap);
+		clustermanager = new ClusterManager(cmanager, null, 0);
+		// load properties to objects
+		loadProperties();
+		updateCorrelation();
+		updateMinCorrelation();
 	}
-
+	
+	public void loadProperties(){
+		// Setup property file location
+		agent = new PropertyAgent("conf");
+		// Load properties
+		coption = Integer.valueOf(agent.getProperties("MGA", "Correlation"));
+		mincorrelation = Double.valueOf(agent.getProperties("MGA", "MinCorrelation"));
+		//
+	}
+	
 	public void updateNodeMap(Map<Integer, Node> nodemap) {
-		this.nodemap = nodemap;
+		cmanager.updateNodeMap(nodemap);
 	}
 
-	public void generateCS() {
-		Iterator<Integer> it1 = nodemap.keySet().iterator();
-		while (it1.hasNext()) {
-			int id1 = it1.next();
-			Map<Integer, Double> subcsmap = new HashMap<Integer, Double>();
-			Iterator<Integer> it2 = nodemap.keySet().iterator();
-			while (it2.hasNext()) {
-				int id2 = it2.next();
-				if (id1 == id2) {
-					continue;
-				}
-				double[] values1 = linkedListToDoubleArray(nodemap.get(id1)
-						.values());
-				double[] values2 = linkedListToDoubleArray(nodemap.get(id2)
-						.values());
-				double correlation = Stat.corrCoeff(values1, values2);
-				subcsmap.put(id2, correlation);
-			}
-			correlationmap.put(id1, subcsmap);
+	public void updateCorrelation(){
+		Correlation c;
+		switch(this.coption){
+			case 1:
+				c = new SimpleCorrelation();
+				break;
+			default:
+				c = new SimpleCorrelation();
+				break;
 		}
+		clustermanager.updateCorrelation(c);
+	}
+	
+	public void updateCorerlation(Correlation c){
+		clustermanager.updateCorrelation(c);
+	}
+	
+	public void updateMinCorrelation(){
+		clustermanager.updateMinCorrelation(this.mincorrelation);
+	}
+	
+	public void updateMinCorrelation(double mincorrelation) {
+		clustermanager.updateMinCorrelation(mincorrelation);
 	}
 
-	public void clustering() {
-
+	// ------------------ Main Functions ---------------------/
+	// Clustering for "level" times. The cluster size is 2^level
+	public Map<Integer, LinkedList<Integer>> clustering(int level){
+		Map<Integer, LinkedList<Integer>> cluster = cmanager.getInitCluster();
+		for(int i = level; i >= 0 ; i--){
+			cluster = clustering(cluster);
+		}
+		return cluster;
+	}
+	
+	// Perform one MGA clustering algorithm. Each clustering level can be put into this function.
+	public Map<Integer, LinkedList<Integer>> clustering(Map<Integer, LinkedList<Integer>> cluster) {
+		return pairing(cluster, clustermanager.getClusterGrade(cluster));
 	}
 
-	public Map<Integer, LinkedList<Integer>> pairing(
+	// ------------------------------------------------------------ /
+	// TODO current version does not consider the minimum requirement of
+	// correlation strength between clusters
+	private Map<Integer, LinkedList<Integer>> pairing(
 			Map<Integer, LinkedList<Integer>> cluster,
 			Map<Integer, LinkedList<Integer>> clustergrade) {
 		Map<Integer, LinkedList<Integer>> pairings = new HashMap<Integer, LinkedList<Integer>>();
 		int index = 0;
-		while (clustergrade.size() != 0) {
+		while (clustergrade.size() > 1) {
 			serachPair(cluster, clustergrade, pairings, index);
+		}
+		if (clustergrade.size() != 0) {
+			logger.info(clustergrade.size() + " residual cluster exist");
+			// TODO pair the residuals with other clustered clusters.
 		}
 		return pairings;
 	}
 
+	// The main function that iterate through the whole cluster until all
+	// clusters are paired
 	private void serachPair(Map<Integer, LinkedList<Integer>> cluster,
 			Map<Integer, LinkedList<Integer>> clustergrade,
 			Map<Integer, LinkedList<Integer>> pairings, int index) {
 		Iterator<Integer> it = clustergrade.keySet().iterator();
 		while (it.hasNext()) {
 			int id = it.next();
+			// If cluster 1 has no any other correlated cluster
 			if (clustergrade.get(id).peek() == null) {
 				clustergrade.get(id).remove();
 				continue;
 			}
+			// If cluster 1 has no any other correlated cluster
 			int id_2 = clustergrade.get(id).peek();
 			if (clustergrade.get(id_2).peek() == null) {
 				continue;
 			}
+
 			// Check whether the first priority of nodeid_1 is successfully
 			// paired.
 			if (id == clustergrade.get(id_2).peek()) {
@@ -86,80 +135,4 @@ public class MGA {
 	}
 
 	// Sort each node with their own priority
-
-	// TODO the grading function
-	private Map<Integer, LinkedList<Integer>> getClusterGrade(
-			Map<Integer, LinkedList<Integer>> cluster) {
-		// ---------- Data structures ----------
-		Map<Integer, Map<Integer, Double>> clustercorrelation = new HashMap<Integer, Map<Integer,Double>>();
-		Map<Integer, LinkedList<Integer>> clustergrade = new HashMap<Integer, LinkedList<Integer>>();
-		// --------------------------------------
-		//----------- Generates Cluster Correlation Map ------------- 
-		Iterator<Integer> it1 = cluster.keySet().iterator();
-		while (it1.hasNext()) {
-			int cluster1 = it1.next();
-			Iterator<Integer> it2 = cluster.keySet().iterator();
-			Map<Integer, Double> subcmap = new HashMap<Integer, Double>();
-			while(it2.hasNext()){
-				int cluster2 = it2.next();
-				if(cluster1 != cluster2){
-					subcmap.put(cluster2, calcCorrelation(cluster.get(cluster1), cluster.get(cluster2)));
-				}
-			}
-			clustercorrelation.put(cluster1, subcmap);
-		}
-		// ----------------------------------------------------------
-		// ------------- Cluster Correlation Grading ----------------
-		
-		// ----------------------------------------------------------
-		return clustergrade;
-	}
-	
-	// Simply accumulate the correlation between each nodes in the two clusters.
-	// TODO May need to include further requirements.
-	private double calcCorrelation(LinkedList<Integer> cluster1, LinkedList<Integer> cluster2){
-		double result = 0;
-		Iterator<Integer> it1 = cluster1.iterator();
-		while(it1.hasNext()){
-			int nodeid1 = it1.next();
-			Iterator<Integer> it2 = cluster2.iterator();
-			while(it2.hasNext()){
-				int nodeid2 = it2.next();
-				result += correlationmap.get(nodeid1).get(nodeid2);
-			}
-		}
-		return result;
-	}
-
-	// Sort Map in a Decent order
-	private LinkedList<Integer> sortGrade(Map<Integer, Double> clustergrade) {
-		LinkedList<Integer> list = new LinkedList<Integer>();
-		Map<Double, Integer> invertgrade = new HashMap<Double, Integer>();
-		// invert grade exchanges the keys & values of clustergrade
-		double[] correlation = new double[clustergrade.size()];
-		int i = 0;
-		Iterator<Integer> it = clustergrade.keySet().iterator();
-		while (it.hasNext()) {
-			int clusterid = it.next();
-			invertgrade.put(clustergrade.get(clusterid), clusterid);
-			correlation[i] = clustergrade.get(clusterid);
-			i++;
-		}
-		// Sort the correlation array in an ascent order
-		Arrays.sort(correlation);
-		// Put node id with regard of the value of correlation in a decent order
-		for (int j = (correlation.length - 1); j >= 0; j--) {
-			list.add(invertgrade.get(correlation[j]));
-		}
-		return list;
-	}
-
-	private double[] linkedListToDoubleArray(LinkedList<Double> list) {
-		double[] array = new double[list.size()];
-		for (int index = 0; index < list.size(); index++) {
-			array[index] = list.get(index);
-		}
-		return array;
-	}
-
 }
---
+++ trunk/CMGA/src/mga/core/SimpleCorrelation.java
@@ -0,0 +1,32 @@
+package mga.core;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class SimpleCorrelation implements Correlation {
+
+	@Override
+	public boolean hasLink(NodeManager nmanager, LinkedList<Integer> cluster1,
+			LinkedList<Integer> cluster2, double mincorrelation) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public double calcCorrelation(NodeManager nmanager,
+			LinkedList<Integer> cluster1, LinkedList<Integer> cluster2,
+			double mincorrelation) {
+		double result = 0;
+		Iterator<Integer> it1 = cluster1.iterator();
+		while (it1.hasNext()) {
+			int nodeid1 = it1.next();
+			Iterator<Integer> it2 = cluster2.iterator();
+			while (it2.hasNext()) {
+				int nodeid2 = it2.next();
+				result += nmanager.getCorrelation(nodeid1, nodeid2);
+			}
+		}
+		return result;
+	}
+
+}
